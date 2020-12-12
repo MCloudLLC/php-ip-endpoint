bb09a578d0e490230ba87205bbdb54a679a4df94
Christopher J. Maldonado <christopher@maldonadocloud.com>
2020-12-12 12:31:29 -0600

    fix for Azure App Services

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && isset($_SERVER['APPSETTING_WEBSITE_SITE_NAME'])) // Azure App Services Endpoint
-	echo $_SERVER['REMOTE_ADDR'];
+// Azure App Services Endpoint
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && isset($_ENV['APPSETTING_WEBSITE_SITE_NAME'])) {
+	if(isset($_SERVER['HTTP_X_CLIENT_IP'])) {
+		echo $_SERVER['HTTP_X_CLIENT_IP'];
+	} else {
+		echo $_SERVER['REMOTE_ADDR'];
+	}
+}
 else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
 	echo $_SERVER['HTTP_X_FORWARDED_FOR'];
 else
